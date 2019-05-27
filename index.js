bc8f6b8fecaf8a5ec6393297146b5ebb75708202
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-27 14:59:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ function BoardMember(name, homeState, training){
  
   
    
-   this.releasePressStatement = function(){
-     return "You will see great things from Scuber."
-   }
-
+   
    this.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
@@ -25,4 +22,8 @@ function BoardMember(name, homeState, training){
  
  BoardMember.prototype.doCharity = function() {
    return "I like to help people"
+ }
+ 
+ BoardMember.prototype.releasePressStatement = function() {
+   return "You will see great things from Scuber"
  }
